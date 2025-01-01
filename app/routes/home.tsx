9d79a63239427cc0f0573c64cf57c67ffe7a3fed
Jenia Brook <jeniabrook@gmail.com>
2025-01-01 23:17:04 +0200

    Layout and nav bar scaffolding

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,6 @@
+import { TerminalIcon } from "~/components/icons/terminal";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { Link } from "react-router";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,5 +10,31 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <div className="grid auto-rows-min min-h-svh">
+      <header className="bg-main border-b-2 border-border sticky top-0">
+        <div className="flex items-center justify-between px-4 h-12">
+          <nav>
+            <Link className="group flex items-center gap-4" to={"/"}>
+              <div className="flex items-center justify-center size-8 bg-accent transition-colors rounded shadow group-hover:bg-interactive">
+                <TerminalIcon strokeWidth="3" />
+              </div>
+              jenia.codes
+            </Link>
+          </nav>
+          <div>Some other stuff</div>
+        </div>
+      </header>
+      <main className="container mx-auto px-4">
+        {Array.from(Array(100).keys()).map(() => (
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Necessitatibus totam sapiente nisi magnam aliquam assumenda maiores
+            voluptatibus sequi autem voluptate nostrum molestiae, sed facilis
+            error dicta quibusdam voluptatum. Necessitatibus, dolorum?
+          </p>
+        ))}
+      </main>
+    </div>
+  );
 }
