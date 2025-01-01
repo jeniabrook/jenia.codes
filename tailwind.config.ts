5602723969b8990d5598989933a64b6ad8ed98a1
Jenia Brook <jeniabrook@gmail.com>
2025-01-01 23:15:45 +0200

    Add palette colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,38 @@ export default {
   content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      colors: {
+        main: "var(--main)",
+        accent: "var(--accent)",
+        interactive: "var(--interactive)",
+        overlay: "var(--overlay)",
+        bg: "var(--bg)",
+        bw: "var(--bw)",
+        blank: "var(--blank)",
+        text: "var(--text)",
+        mtext: "var(--mtext)",
+        border: "var(--border)",
+        ring: "var(--ring)",
+        ringOffset: "var(--ring-offset)",
+        secondaryBlack: "#212121",
+      },
+      borderRadius: {
+        DEFAULT: "4px",
+      },
+      boxShadow: {
+        DEFAULT: "var(--shadow)",
+      },
+      translate: {
+        boxShadowX: "4px",
+        boxShadowY: "4px",
+        reverseBoxShadowX: "-4px",
+        reverseBoxShadowY: "-4px",
+      },
+      fontWeight: {
+        base: "500",
+        heading: "700",
+      },
+
       fontFamily: {
         sans: [
           '"Inter"',
