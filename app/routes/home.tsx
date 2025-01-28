912d55e8242a14a8336fe501c570eb6f2cdfaba8
Jenia Brook <jeniabrook@gmail.com>
2025-01-28 21:51:33 +0200

    Small improvements

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -25,11 +25,10 @@ export default function Home() {
               jenia.codes
             </Link>
           </nav>
-          <div>Some other stuff</div>
         </div>
       </header>
       <main className="container mx-auto flex flex-col flex-1 px-4">
-        <section className="grid gap-10 py-8 sm:gap-4 sm:grid-cols-2 sm:py-40">
+        <section className="grid gap-4 py-8 md:grid-cols-2 md:py-40">
           <div className="text-balance">
             <h3 className="text-2xl mb-3">Hi, my name is Jenia.</h3>
             <h1 className="font-title font-bold text-7xl mb-3">
@@ -40,21 +39,17 @@ export default function Home() {
             </h3>
           </div>
           <div className="group place-self-center grid p-9">
-            <div className="flex justify-center items-center col-start-1 z-3 row-start-1 transition-all duration-200 rotate-12 origin-bottom-right size-52 bg-accent rounded-md shadow group-hover:rotate-0">
+            <div className="flex justify-center items-center col-start-1 z-3 row-start-1 transition-all duration-200 group-hover:rotate-12 origin-bottom-right size-52 bg-accent rounded-md shadow rotate-0">
               <img
-                className="size-48 rounded-md grayscale group-hover:grayscale-0 transition-all duration-200"
+                className="size-44 rounded-md grayscale group-hover:grayscale-0 transition-all duration-200"
                 src="/me.jpg"
                 alt="Jenia Brook"
               />
             </div>
             <div className="col-start-1 z-2 row-start-1 size-52 bg-interactive rounded-md shadow"></div>
-            <div className="col-start-1 row-start-1 size-52 transition-all duration-200 -rotate-12 origin-bottom-right z-0 bg-main rounded-md shadow group-hover:rotate-0"></div>
+            <div className="col-start-1 row-start-1 size-52 transition-all duration-200 group-hover:-rotate-12 origin-bottom-right z-0 bg-main rounded-md shadow rotate-0"></div>
           </div>
         </section>
-        <section>Some</section>
-        <section>Some</section>
-        <section>Some</section>
-        <section>Some</section>
       </main>
     </div>
   );
