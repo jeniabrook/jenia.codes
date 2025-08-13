1ed9947fba3ba43714062ec8bf1ab74615532254
Jenia Brook <jenia@helloflare.com>
2025-08-13 19:17:41 +0300

    Remove Icon and TerminalIcon components from the project

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,13 +1,11 @@
 import { Link } from "react-router";
-import { TerminalIcon } from "./icons/terminal";
 
 export function Navbar() {
   return (
     <header className="bg-main border-b border-border sticky top-0 z-50">
       <div className="flex items-center justify-between px-4 h-14">
         <nav>
-          <Link className="group flex items-center select-none" to={"/"}>
-            <TerminalIcon />
+          <Link className="flex items-center select-none" to={"/"}>
             <span className="font-mono">jenia.codes</span>
           </Link>
         </nav>
